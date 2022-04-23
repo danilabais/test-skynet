89c85308994e35d3f718f80904b0d330d552d3b1
Danila <da_buddha@bk.ru>
2022-04-23 23:54:32 +0500

    change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <hr>
+    <h4 class="title">
+      Тип подчключения:
+    </h4>
+    <div class="card">
+      <div class="card__left">
+        <h3 class="card__title">
+          {{data[0].variants[0].title}}
+        </h3>
+        <div v-for="(item,key) in data[0].variants[0].description.split('\n')" :key="key">
+        <p>
+          {{item}}
+        </p>
+
+        </div>
+      </div>
+      <div class="card__right">
+        <span class="card__price">
+          400 Р
+        </span>
+        <form class="card__form">
+          <div  class="card__input-cable input" v-for="(item,key) in data[0].variants[0].options" :key="key">
+            <input v-model="input[key]" type="checkbox" :id="key">
+            <label :for="key">{{item.title}}</label>
+          </div>
+          
+          <button v-on:click.prevent="click" class="card__submit">Выбрано</button>
+        </form>
+      </div>
+    </div>
   </div>
+  <pre>
+    {{data}}
+  </pre>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import db from "@/assets/db/db.json"
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    data(){
+      return {
+        data:db,
+        input:{},
+      }
+    },
+    methods: {
+      click() {
+        console.log(this.input)
+      }  
+    },
+    computed :{
+      test(){
+        console.log(data[0].variants[0].options[this.input])
+      }
+    },
+    watch: {
+      input: {
+            deep: true,
+            handler(newValue, oldValue) {
+                this.click()
+            }
+        }
+    
+    }
 }
 </script>
+
+<style>
+
+</style>
